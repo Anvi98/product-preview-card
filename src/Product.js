bd64a03549a70a8570677ee81723a1a3c84be5a9
Anvi Alex <epononanvialex@gmail.com>
2023-02-01 12:10:09 +0200

    adding color Theme in tailwind

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,9 @@
+import perfume from "./assets/image-product-mobile.jpg"
 
 export const Product = () => {
   return ( 
     <div>
+      <img src={perfume} alt="perfume" />
       <h1 className="underline text-lg font-bold">Hello</h1>
     </div>
    );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-cyan': 'hsl(158, 36%, 37%)',
+        'Cream' : 'hsl(30, 38%, 92%)',
+        
+        'grayish-blue': 'hsl(228, 12%, 48%)',
+        'dark blue':'hsl(212, 21%, 14%)',
+        'white': 'hsl(0, 0%, 100%)'
+      },
+    },
   },
   plugins: [],
 }
